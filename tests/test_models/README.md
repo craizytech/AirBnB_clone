b7f9b24db8ba2387814196359692844bf319e03a
craizytech <craizytech@gmail.com>
2024-04-14 11:08:25 +0300

    wrote the tests for the base model

---
+++ tests/test_models/README.md
@@ -0,0 +1,1 @@
+ This folder contains the unittests for the AirBnB clone project
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This is the base_model test module."""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Testing the base class module."""
+
+    def setUp(self):
+        """sets up all the required items for objects to work."""
+        self.obj1 = BaseModel()
+        self.obj2 = BaseModel()
+
+    def test_uuid(self):
+        """This method tests the id attribute."""
+        self.assertIsInstance(self.obj1, BaseModel)
+        self.assertNotEqual(self.obj1.id, self.obj2.id)
+
+    def str_method(self):
+        """Tests the str method."""
+        self.assertEqual(str(self.obj1),\
+                f"[BaseModel] ({self.obj1.id}) {self.obj1.__dict__}")
+        self.assertEqual(str(self.obj2),\
+                f"[BaseModel] ({self.obj1.id}) {self.obj1.__dict__}")
+
+    def test_save(self):
+        """Tests the save method."""
+        self.assertIs(self.obj1.updated_at, None)
+        self.obj1.save()
+        self.assertIsNot(self.obj1.updated_at, None)
