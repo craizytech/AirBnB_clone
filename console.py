1d4c068c27bb15622b5f7bb6fb5ae1dff8220bec
craizytech <craizytech@gmail.com>
2024-04-16 16:12:08 +0300

    fixed some EOF issues

--- console.py
+++ console.py
@@ -7,11 +7,11 @@ class HBNBCommand(cmd.Cmd):
     """This class creates a console for the HBNB."""
     prompt = '(hbnb) '
 
-    def do_quit(self):
+    def do_quit(self, line):
         """This method implements the quit command."""
         return True
-    def do_EOF(self):
-        """This implements the EOF."""
+    def do_EOF(self, line):
+        """Indicates the EOF."""
         return True
     def emptyline(self):
         pass
