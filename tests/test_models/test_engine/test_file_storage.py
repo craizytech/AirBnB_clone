294e265e4c212d5f85809b9e11d1ba288406c4af
craizytech <craizytech@gmail.com>
2024-04-20 14:24:18 +0300

    Wrote some tests for the file_storage file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,21 +5,21 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
-class TestFileStorage:
+class TestFileStorage(unittest.TestCase):
     """Testing the file storage module."""
 
     def setUp(self):
         """Sets up all the objects needed for testing"""
-        f1_obj = FileStorage()
-        f2_obj = FileStorage()
-        b1_obj = BaseModel()
+        self.f1_obj = FileStorage()
+        self.f2_obj = FileStorage()
+        self.b1_obj = BaseModel()
 
     def test_instance(self):
         """Tests the instance of FileStorage."""
-        self.assertIsInstance(f1_obj, FileStorage)
+        self.assertIsInstance(self.f1_obj, FileStorage)
 
     def test_attributes(self):
         """Tests the private class attributes of the class."""
         with self.assertRaises(AttributeError):
-            f1_obj.file_path
-        self.assertIsEqual(f1_0bj_FileStorage__file_path, "file.json")
+            self.f1_obj.file_path
+        self.assertEqual(self.f1_obj._FileStorage__file_path, "file.json")
